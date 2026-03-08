f5dc9d81e2226ad7a4bd827962f8b812c7405b66
Silvio-Hub <silviocelso.dev@gmail.com>
2026-03-08 17:02:13 -0300

    APK criando arquivo .env dummy antes da compilação

--- lib/app/main_development.dart
+++ lib/app/main_development.dart
@@ -7,10 +7,15 @@ import 'package:mindease/presentation/controllers/accessibility_cubit.dart';
 import 'package:mindease/presentation/pages/splash_page.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
-  await setupDependencies();
-  runApp(const MindEaseDevApp());
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    await dotenv.load(fileName: ".env");
+    await setupDependencies();
+    runApp(const MindEaseDevApp());
+  } catch (e, stackTrace) {
+    debugPrint('Erro fatal na inicialização: $e');
+    debugPrint(stackTrace.toString());
+  }
 }
 
 class MindEaseDevApp extends StatelessWidget {
